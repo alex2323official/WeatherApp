30398a4cd8e32450486ab6c510fdde6779c86e4f
Alex <alex2323official@gmail.com>
2024-10-16 10:45:49 +0200

    Add JS model fetch doing API call and reciving object with needed data for view

--- App/Model/ApiModel.js
+++ App/Model/ApiModel.js
@@ -8,10 +8,22 @@ export class ApiModel {
     // Call Visual Crossing API
     try {
       const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=2U5JV7AQJVKUV3U8J2AQS4934&contentType=json`
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=2U5JV7AQJVKUV3U8J2AQS4934&contentType=json`
       );
       const data = await response.json();
-      console.log(data);
+
+      // Return Object with needed data (pass it to View)
+      let dataForDisplay = {
+        fullAddress: data.resolvedAddress,
+        description: data.days[0].conditions,
+        currentDate: data.days[0].datetime,
+        currentTemp: data.days[0].temp,
+        feelsLikeTemp: data.days[0].feelslike,
+        humidity: data.days[0].humidity,
+        windSpeed: data.days[0].windspeed,
+      };
+      console.table(dataForDisplay);
+      return dataForDisplay;
     } catch (error) {
       console.error("Error fetching data: ", error);
     }
