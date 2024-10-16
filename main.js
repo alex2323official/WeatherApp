a6101dbdc6521d936c28d8532813e368c34f24a0
Alex <alex2323official@gmail.com>
2024-10-16 10:02:33 +0200

    Add Controller JS file

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import "./style.css";
+import { MainController } from "./App/Controller/MainController.js";
+
+// App starts here
+const mainController = new MainController();
+mainController.listenForCityInput();
