32dc70bd67440d2225d802d4daf254892a0c87f6
Alex <alex2323official@gmail.com>
2024-10-16 12:42:06 +0200

    In Display we are showing a data in UI

--- App/View/Display.js
+++ App/View/Display.js
@@ -5,12 +5,20 @@ export class Display {
   #todayAddress = document.querySelector("#todayAddress");
   #todayDate = document.querySelector("#todayDate");
   #todayDescription = document.querySelector("#todayDescription");
-  #todayFeelsLike = document.querySelector("#todayFeelsLike");
+  #todayFeelsLike = document.querySelector("#todayFeelsLikeTemp");
   #todayHumidity = document.querySelector("#todayHumidity");
   #todayWindSpeed = document.querySelector("#todayWindSpeed");
 
   displayUi(data) {
     let correctData = JSON.parse(data);
-    console.log(correctData.fullAddress);
+    console.log(correctData);
+
+    this.#todayTemp.textContent = correctData.currentTemp;
+    this.#todayAddress.textContent = correctData.fullAddress;
+    this.#todayDate.textContent = correctData.currentDate;
+    this.#todayDescription.textContent = correctData.description;
+    this.#todayFeelsLike.textContent = correctData.feelsLikeTemp;
+    this.#todayHumidity.textContent = correctData.humidity;
+    this.#todayWindSpeed.textContent = correctData.windSpeed;
   }
 }
