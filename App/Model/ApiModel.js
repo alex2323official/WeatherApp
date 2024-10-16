301201658f7c78c097c9f5b30dee715d2acf020d
Alex <alex2323official@gmail.com>
2024-10-16 10:28:39 +0200

    Api call in Model works

--- App/Model/ApiModel.js
+++ App/Model/ApiModel.js
@@ -1,5 +1,19 @@
 export class ApiModel {
   callWeatherApi(city) {
     console.log(city);
+    this.fetchApiCall(city);
+  }
+
+  async fetchApiCall(city) {
+    // Call Visual Crossing API
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=2U5JV7AQJVKUV3U8J2AQS4934&contentType=json`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
   }
 }
